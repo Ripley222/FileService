618834a209a8e093806342cf00f498d8bad76efb
Nikita Petrunyak <ox1om2002@gmail.com>
2025-12-18 12:43:47 +0300

    создан Factory доменной области

---
+++ FIleService/src/FileService.Domain/Factory/IMediaAssetFactory.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using FileService.Domain.Entities;
+using FileService.Domain.ValueObjects;
+using Shared.SharedKernel.Errors;
+
+namespace FileService.Domain.Factory;
+
+public interface IMediaAssetFactory
+{
+    Result<VideoAsset, Error> CreateVideoForUpload(MediaData mediaData, MediaOwner owner);
+    
+    Result<PreviewAsset, Error> CreatePreviewForUpload(MediaData mediaData, MediaOwner owner);
+}
