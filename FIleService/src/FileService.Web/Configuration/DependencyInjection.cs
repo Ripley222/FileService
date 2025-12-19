0a26027be9f1370f5703dc223257a92a71738c60
Nikita Petrunyak <ox1om2002@gmail.com>
2025-12-19 19:43:57 +0300

    рефакторинг

--- FIleService/src/FileService.Web/Configuration/DependencyInjection.cs
+++ FIleService/src/FileService.Web/Configuration/DependencyInjection.cs
@@ -3,7 +3,7 @@ using Serilog.Exceptions;
 
 namespace FileService.Web.Configuration;
 
-public static class DependencyInjectionExtensions
+public static class DependencyInjection
 {
     public static IServiceCollection AddConfiguration(
         this IServiceCollection services, IConfiguration configuration)
