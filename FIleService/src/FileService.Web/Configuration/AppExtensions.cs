8b5ab9d047263810b021698caafca1c96e26dcc2
Nikita Petrunyak <ox1om2002@gmail.com>
2025-12-26 17:45:54 +0300

    рефакторинг

--- FIleService/src/FileService.Web/Configuration/AppExtensions.cs
+++ FIleService/src/FileService.Web/Configuration/AppExtensions.cs
@@ -1,18 +1,20 @@
-﻿using Serilog;
+﻿using FileService.Core.Endpoints;
+using Serilog;
 using Shared.Framework.Extensions;
 
 namespace FileService.Web.Configuration;
 
 public static class AppExtensions
 {
-    public static IApplicationBuilder Configure(this IApplicationBuilder app)
+    public static IApplicationBuilder Configure(this WebApplication app)
     {
         app.UseExceptionMiddleware();
-        
         app.UseSerilogRequestLogging();
-
         app.UseSwagger();
         app.UseSwaggerUI();
+        
+        var apiGroup = app.MapGroup("/file-service").WithOpenApi();
+        app.MapEndpoints(apiGroup);
 
         return app;
     }
