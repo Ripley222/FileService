cd8c0c8556ddc6d7d1f4c06c195838ed52150046
Nikita Petrunyak <ox1om2002@gmail.com>
2025-12-26 17:45:29 +0300

    реализация фич для работы с методами S3 провайдера

---
+++ FIleService/src/FileService.Core/Features/GenerateDownloadUrls.cs
@@ -0,0 +1,87 @@
+﻿using CSharpFunctionalExtensions;
+using FileService.Core.Endpoints;
+using FileService.Core.FileProviders;
+using FileService.Core.Repositories;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Shared.Core.Validation;
+using Shared.SharedKernel.Errors;
+
+namespace FileService.Core.Features;
+
+public sealed record DownloadPresignedUrlsRequest(IEnumerable<Guid> FileIds);
+
+public sealed class DownloadPresignedUrlsRequestValidator : AbstractValidator<DownloadPresignedUrlsRequest>
+{
+    public DownloadPresignedUrlsRequestValidator()
+    {
+        RuleForEach(d => d.FileIds)
+            .Must(id => id != Guid.Empty)
+            .WithError(Errors.General.ValueIsInvalid("FileId"));
+    }
+}
+
+public sealed class GenerateDownloadUrlsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("download-urls", async (
+            [FromQuery] Guid[] fileIds,
+            [FromServices] GeneratePresignedUrlsHandler presignedUrlsHandler,
+            CancellationToken cancellationToken) =>
+        {
+            var result = await presignedUrlsHandler
+                .Handle(new DownloadPresignedUrlsRequest(fileIds), cancellationToken);
+
+            return Results.Ok(result.Value);
+        }).DisableAntiforgery();
+    }
+}
+
+public sealed class GeneratePresignedUrlsHandler
+{
+    private readonly IS3Provider _s3Provider;
+    private readonly IMediaRepository _mediaRepository;
+    private readonly IValidator<DownloadPresignedUrlsRequest> _validator;
+    private readonly ILogger<GeneratePresignedUrlHandler> _logger;
+
+    public GeneratePresignedUrlsHandler(
+        IS3Provider s3Provider,
+        IMediaRepository mediaRepository,
+        IValidator<DownloadPresignedUrlsRequest> validator,
+        ILogger<GeneratePresignedUrlHandler> logger)
+    {
+        _s3Provider = s3Provider;
+        _mediaRepository = mediaRepository;
+        _validator = validator;
+        _logger = logger;
+    }
+
+    public async Task<Result<IReadOnlyList<string>, ErrorList>> Handle(
+        DownloadPresignedUrlsRequest request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (validationResult.IsValid is false)
+            return validationResult.GetErrors();
+
+        var mediaAssetsResult = await _mediaRepository.GetByIdsAsync(request.FileIds, cancellationToken);
+        if (mediaAssetsResult.IsFailure)
+            return mediaAssetsResult.Error.ToErrors();
+
+        var keys = mediaAssetsResult.Value.Select(m => m.RawKey!.IsEmpty() 
+            ? m.FinalKey
+            : m.RawKey).ToList();
+
+        var presignedUrlResult = await _s3Provider.GenerateDownloadUrlsAsync(keys!);
+        if (presignedUrlResult.IsFailure)
+            return presignedUrlResult.Error.ToErrors();
+
+        _logger.LogInformation("Generated download URLs for many files.");
+
+        return Result.Success<IReadOnlyList<string>, ErrorList>(presignedUrlResult.Value);
+    }
+}
---
+++ FIleService/src/FileService.Core/Features/RemoveFile.cs
@@ -0,0 +1,95 @@
+﻿using CSharpFunctionalExtensions;
+using FileService.Core.Endpoints;
+using FileService.Core.FileProviders;
+using FileService.Core.Repositories;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Shared.Core.Validation;
+using Shared.SharedKernel.Errors;
+
+namespace FileService.Core.Features;
+
+public sealed record RemoveFileRequest(Guid FileId);
+
+public sealed class RemoveFileRequestValidator : AbstractValidator<RemoveFileRequest>
+{
+    public RemoveFileRequestValidator()
+    {
+        RuleFor(x => x.FileId)
+            .Must(id => id != Guid.Empty)
+            .WithError(Errors.General.ValueIsInvalid("FileId"));
+    }
+}
+
+public sealed class RemoveEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("remove", async (
+                [FromQuery] Guid fileId,
+                [FromServices] DeleteFileHandler handler,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await handler.Handle(new RemoveFileRequest(fileId), cancellationToken);
+
+                return Results.Ok(result.Value);
+            })
+            .DisableAntiforgery();
+    }
+}
+
+public sealed class DeleteFileHandler
+{
+    private readonly IS3Provider _s3Provider;
+    private readonly IMediaRepository _mediaRepository;
+    private readonly IValidator<RemoveFileRequest> _validator;
+    private readonly ILogger<DeleteFileHandler> _logger;
+
+    public DeleteFileHandler(
+        IS3Provider s3Provider,
+        IMediaRepository mediaRepository,
+        IValidator<RemoveFileRequest> validator,
+        ILogger<DeleteFileHandler> logger)
+    {
+        _s3Provider = s3Provider;
+        _mediaRepository = mediaRepository;
+        _validator = validator;
+        _logger = logger;
+    }
+
+    public async Task<Result<string, ErrorList>> Handle(
+        RemoveFileRequest request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (validationResult.IsValid is false)
+            return validationResult.GetErrors();
+        
+        var mediaAssetResult = await _mediaRepository.GetByIdAsync(request.FileId, cancellationToken);
+        if (mediaAssetResult.IsFailure)
+            return mediaAssetResult.Error.ToErrors();
+
+        var key = mediaAssetResult.Value.RawKey!.IsEmpty()
+            ? mediaAssetResult.Value.FinalKey
+            : mediaAssetResult.Value.RawKey;
+
+        var removeFileInS3Result = await _s3Provider.RemoveFileAsync(key!, cancellationToken);
+        if (removeFileInS3Result.IsFailure)
+            return removeFileInS3Result.Error.ToErrors();
+        
+        var markDeletedResult = mediaAssetResult.Value.MarkDeleted(DateTime.UtcNow);
+        if (markDeletedResult.IsFailure)
+            return markDeletedResult.Error.ToErrors();
+
+        var saveChangesResult = await _mediaRepository.SaveChangesAsync(cancellationToken);
+        if (saveChangesResult.IsFailure)
+            return saveChangesResult.Error.ToErrors();
+
+        _logger.LogInformation("File with id {fileId} deleted.", request.FileId);
+
+        return removeFileInS3Result.Value;
+    }
+}
