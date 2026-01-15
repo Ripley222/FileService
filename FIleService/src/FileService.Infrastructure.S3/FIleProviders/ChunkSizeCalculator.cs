5a2af5ec85ca3c35900139a24f61a205e0c7ceb4
Nikita Petrunyak <ox1om2002@gmail.com>
2026-01-15 17:18:31 +0300

    реализация ChunkSizeCalculator и соответствующих настроек

---
+++ FIleService/src/FileService.Infrastructure.S3/FIleProviders/ChunkSizeCalculator.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+using FileService.Core.FileProviders;
+using Shared.SharedKernel.Errors;
+
+namespace FileService.Infrastructure.S3.FIleProviders;
+
+public class ChunkSizeCalculator : IChunkSizeCalculator
+{
+    public Result<(long ChunkSize, int TotalChunks), Error> ChunksCalculator(
+        long fileSize,
+        int recommendedChunkSize,
+        int maxChunks)
+    {
+        if (fileSize < recommendedChunkSize)
+            return (fileSize, 1);
+
+        int chunkSize = recommendedChunkSize;
+
+        while (true)
+        {
+            int calculatedChunkSize = (int)Math.Ceiling((double)fileSize / recommendedChunkSize);
+            if (calculatedChunkSize <= maxChunks)
+                return (chunkSize, calculatedChunkSize);
+
+            chunkSize *= 2;
+        }
+    }
+}
